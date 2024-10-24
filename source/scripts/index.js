b1834fcb7f4ba33b70f27312514fc2834c7152d7
Tatiana B <tbeketova04@gmail.com>
2024-10-24 23:02:45 +0300

    added basic mobile style for header and burger-menu, added js open/close menu

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,15 +1,14 @@
+const btnToggle = document.querySelector('.toggle');
 const nav = document.querySelector('.navigation');
-const navToggle = document.querySelector('.navigation__toggle');
+
 nav.classList.remove('navigation--nojs');
 
-navToggle.addEventListener('click', () => {
-  //console.log('Кнопка нажата');
-  if (nav.classList.contains('navigation--closed')) {
+btnToggle.addEventListener('click', () => {
+  if(nav.classList.contains('navigation--closed')) {
     nav.classList.remove('navigation--closed');
     nav.classList.add('navigation--opened');
   } else {
     nav.classList.add('navigation--closed');
     nav.classList.remove('navigation--opened');
   }
-  //console.log(nav.classList);
 });
